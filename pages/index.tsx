4d1ccd0088201684c72be58ba5c267b581af2197
rakategar <tegarrakaw@gmail.com>
2024-05-30 21:00:08 +0700

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -120,8 +120,8 @@ export default function Home() {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://anaflous.com`} />
-        <link rel="canonical" href={`https://anaflous.com`} />
+        <meta property="og:url" content={`https://portoraka.site`} />
+        <link rel="canonical" href={`https://portoraka.site`} />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Manu Arora" />
         <meta property="og:description" content={meta.description} />
