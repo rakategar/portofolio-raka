caed700efb17fa581ed4b5b01d2a9a8fa7d27a47
rakategar <tegarrakaw@gmail.com>
2024-06-28 17:40:49 +0700

    modified:   components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx 	modified:   package-lock.json 	new file:   public/lovemeter.png

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -726,12 +726,91 @@ export default function SomethingIveBuilt() {
               </ul>
               <div className="z-10 flex fle-row space-x-5 sm:pt-0 pt-2">
                 <GithubIcon link="https://github.com/rakategar/web-cek-kodam" />
+                <a href="https://cek-kodam-xi.vercel.app/" target={"_blank"} rel="noreferrer">
+                  <ExternalLink url={""} router={router} />
+                </a>
               </div>
             </div>
           </div>
           
         </div>
       
+
+        {/* // ? Project 11 */}
+        <div data-aos="fade-up" className="relative  md:grid md:grid-cols-12 w-full md:h-96">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              <a href="https://love-meter-coral.vercel.app/" target={"_blank"} rel="noreferrer">
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAprimary 
+           transition-opacity opacity-50 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </a>
+
+              <div className="absolute px-2 bg-AAprimary rounded  ml-4 mt-2 ">
+                
+              </div>
+
+              <Img src={"/lovemeter.png"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/lovemeter.png"} alt={"Project Screen shot"} className={`w-full h-full `} />
+              </div>
+            </div>
+            <div className="absolute w-full   rounded  ml-4 mt-2  text-end pr-8">
+              <div className="w-full"></div>
+            </div>
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
+            col-span-8 flex flex-col items-start md:items-end sm:space-y-3 space-y-1"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">Web Dev Project</span>
+                <a href="https://love-meter-coral.vercel.app/" target={"_blank"} rel="noreferrer">
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    Web Love Meter
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
+                <span className="text-AAsecondary text-base">Web Love Meter</span>
+                , merupakan aplikasi website yang akan memunculkan presentase kecocokan dari 2 nama yang di input oleh user,
+                web ini dikemas dalam tampilan lovely
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">NextJS</span>
+                <span className="pr-4 z-10">Tailwind</span>
+                <span className="pr-4 z-10">Daisy UI</span>
+                
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 sm:pt-0 pt-2">
+              <GithubIcon link="https://github.com/rakategar/love-meter" />
+              <a href="https://love-meter-coral.vercel.app/" target={"_blank"} rel="noreferrer">
+                  <ExternalLink url={""} router={router} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
 
     </div>
     </div>
